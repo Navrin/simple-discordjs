9cf1162c960fb114049c3a5531833368ee27e166
Navrin Thomas <navman360@gmail.com>
2017-06-04 23:11:52 +1000

    Export all types from commands

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default } from './commands';
-export { MiddlewareFunction, CommandFunction } from './commands';
+export { MiddlewareFunction, CommandFunction, CommandDefinition } from './commands';
+export * from './commands';
 export { Auth, RateLimiter, RoleTypes } from './middleware';
