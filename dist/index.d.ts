0212bb00f81548c83cee9b68341221ee44aae44c
Navrin <navman360@gmail.com>
2017-06-06 09:05:17 +1000

    Update dist folder

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,4 @@
 export { default } from './commands';
+export { MiddlewareFunction, CommandFunction, CommandDefinition } from './commands';
+export * from './commands';
 export { Auth, RateLimiter, RoleTypes } from './middleware';
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var commands_1 = require("./commands");
 exports.default = commands_1.default;
+__export(require("./commands"));
 var middleware_1 = require("./middleware");
 exports.Auth = middleware_1.Auth;
 exports.RateLimiter = middleware_1.RateLimiter;
