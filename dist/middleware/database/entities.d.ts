3aa14567b032a122c88c9f6c53ce25da2bc3e8b7
Navrin <navman360@gmail.com>
2017-07-11 19:39:56 +1000

    Update Discord.JS

--- dist/middleware/database/entities.d.ts
+++ dist/middleware/database/entities.d.ts
@@ -1,5 +1,5 @@
 import { User } from './user/model';
 import { Guild } from './guild/model';
 import { Role } from './role/model';
-declare const _default: (typeof User | typeof Guild | typeof Role)[];
+declare var _default: (typeof User | typeof Guild | typeof Role)[];
 export default _default;
